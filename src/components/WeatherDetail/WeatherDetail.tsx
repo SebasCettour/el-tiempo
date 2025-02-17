30d9f0b26b62216d5a51467ae848e4e0506ebf34
Sebastián Cettour <sebas.cettour83@gmail.com>
2025-02-17 01:46:57 -0300

    finalizando componente del tiempo

--- src/components/WeatherDetail/WeatherDetail.tsx
+++ src/components/WeatherDetail/WeatherDetail.tsx
@@ -1,5 +1,6 @@
 import { formatTemperature } from "../../helpers";
 import { WeatherSchema } from "../../hooks/useWeather";
+import styles from "./WeatherDetail.module.css";
 
 type WeatherDetailProps = {
   weather: WeatherSchema;
@@ -7,12 +8,24 @@ type WeatherDetailProps = {
 
 export default function WeatherDetail({ weather }: WeatherDetailProps) {
   return (
-    <div>
+    <div className={styles.container}>
       <h2> El tiempo en {weather.name}</h2>
-      <p>Temperatura Actual: {formatTemperature(weather.main.temp)}&deg; C</p>
-      <div>
-        <p>Max: {formatTemperature(weather.main.temp_max)}&deg; C</p>
-        <p>Min: {formatTemperature(weather.main.temp_min)}&deg; C</p>
+      <p className={styles.current}>
+        Temperatura Actual: {formatTemperature(weather.main.temp)}&deg; C
+      </p>
+      <div className={styles.temperatures}>
+        <p className={styles.p_max}>
+          Max:{" "}
+          <span className={styles.span_max}>
+            {formatTemperature(weather.main.temp_max)}&deg; C{" "}
+          </span>
+        </p>
+        <p className={styles.p_min}>
+          Min:{" "}
+          <span className={styles.span_min}>
+            {formatTemperature(weather.main.temp_min)}&deg; C{" "}
+          </span>
+        </p>
       </div>
     </div>
   );
